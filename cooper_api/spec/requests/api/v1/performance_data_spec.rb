ba7d885f0df84fae636920bd8e54dde9340523da
mmollerstrom <mmollerstrom@icloud.com>
2017-05-18 10:36:41 +0200

    add index for performance_data

--- cooper_api/spec/requests/api/v1/performance_data_spec.rb
+++ cooper_api/spec/requests/api/v1/performance_data_spec.rb
@@ -1,25 +1,27 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
   let(:user) { FactoryGirl.create(:user) }
   let(:credentials) { user.create_new_auth_token }
-  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
   describe 'POST /api/v1/performance_data' do
     it 'creates a data entry' do
-      POST '/api/V1/performance_data', params: { performance_data: { data: { message: 'Average' } } }, headers: headers
+      post '/api/v1/performance_data', params: {
+        performance_data: { data: { message: 'Average' } }
+        }, headers: headers
 
       entry = PerformanceData.last
-      expect(entry.data).to eq 'message' => 'Avarage'
+      expect(entry.data).to eq 'message' => 'Average'
     end
   end
 
   describe 'GET /api/v1/performance_data' do
-    before do
-      5.times { user.performance_data.create(data: { message: 'Avarage' }) }
-    end
+      before do
+        5.times { user.performance_data.create(data: { message: 'Average' }) }
+      end
 
     it 'returns a collection of performance data' do
-      GET '/api/v1/performance_data', headers: headers
+      get '/api/v1/performance_data', headers: headers
       expect(response_json['entries'].count).to eq 5
+      end
     end
   end
-end
